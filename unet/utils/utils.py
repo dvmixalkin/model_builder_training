a4458d0ae103d85f7d748884928b36f86f1dfbf4
dvmixalkin <dvmixalkin@gmail.com>
2022-12-07 14:17:42 +0300

    change axis(orientation)

--- unet/utils/utils.py
+++ unet/utils/utils.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 
 def plot_img_and_mask(img, mask):
     classes = mask.shape[0] if len(mask.shape) > 2 else 1
-    fig, ax = plt.subplots(1, classes + 1)
+    # fig, ax = plt.subplots(1, classes + 1)
+    fig, ax = plt.subplots(classes + 1, 1)
     ax[0].set_title('Input image')
     ax[0].imshow(img)
     if classes > 1:
