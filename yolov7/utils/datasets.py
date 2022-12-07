b3e42e05f5400a9e40d3d6972d1e5da2f6e675f5
dvmixalkin <dvmixalkin@gmail.com>
2022-12-07 14:20:46 +0300

    reformat to PEP8 (blank spaces)

--- yolov7/utils/datasets.py
+++ yolov7/utils/datasets.py
@@ -413,7 +413,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
         # ==============================================================================================================
 
         # cache_path = (p if p.is_file() else Path(self.label_files[0]).parent).with_suffix('.cache')
-        cache_path = p.with_suffix('.cache')# cached labels
+        cache_path = p.with_suffix('.cache')  # cached labels
         if cache_path.is_file():
             cache, exists = torch.load(cache_path), True  # load
         else:
