3843eb83350955de8ffb28da4a045a06d41cbb78
dvmixalkin <dvmixalkin@gmail.com>
2022-12-11 19:47:50 +0300

    some fixes

--- yolov7/conveer/utils/opt_checker.py
+++ yolov7/conveer/utils/opt_checker.py
@@ -2,76 +2,6 @@ import os
 import distutils
 
 
-# def check_opts(opt, custom_cfg=None, data_path='data'):
-#     unmatched_configs = custom_cfg
-#
-#     if custom_cfg:
-#         convert_back = False
-#         if isinstance(custom_cfg, str):
-#             with open(custom_cfg, 'r') as stream:
-#                 configs = yaml.safe_load(stream)
-#         elif isinstance(custom_cfg, dict):
-#             configs = custom_cfg
-#         unmatched_configs = {}
-#         if isinstance(opt, dict):
-#             opt = argparse.Namespace(**opt)
-#             convert_back = True
-#         for k, v in configs.items():
-#             k = k.replace('-', '_')
-#             if hasattr(opt, k):
-#                 _type = type(getattr(opt, k))
-#
-#                 if k == 'project':
-#                     if v.startswith('/'):
-#                         v = v[1:]
-#                 try:
-#                     if _type == list:
-#                         content_type = None
-#                         if len(getattr(opt, k)) > 0:
-#                             content_type = type(getattr(opt, k)[0])
-#                         if isinstance(v, list):
-#                             value = v
-#                         else:
-#                             value = [
-#                                 content_type(key) if content_type is not None else key for key in
-#                                 v.strip('][').split(', ') if key != ''
-#                             ]
-#                     elif _type == type(None):
-#                         value = v
-#                     elif _type == bool:
-#                         value = bool(distutils.util.strtobool(v))
-#                     else:
-#                         value = type(getattr(opt, k))(v)
-#
-#                     if k == 'batch_size':
-#                         value = max(1, value)  # '16'
-#                     if k == 'project':
-#                         value = f'{data_path}/{v}'
-#                     if k == 'workers':
-#                         value = max(0, value)
-#                     if k == 'epochs':
-#                         value = max(5, value)
-#                     if k == 'nc':
-#                         value = len(getattr(opt, 'names')) if value == 0 else v
-#                     if k == 'names':
-#                         if len(value) == 0:
-#                             raise ValueError('Class names should be equal to nc!')
-#                     if k == 'train' and value == '':
-#                         value = os.path.join(data_path, 'train_dataset.pkl')
-#                     if k == 'val' and value == '':
-#                         value = os.path.join(data_path, 'val_dataset.pkl')
-#                     if k == 'test' and value == '':
-#                         value = os.path.join(data_path, 'test_dataset.pkl')
-#                     setattr(opt, k, value)
-#                 except:
-#                     raise TypeError('Type mismatch')
-#             else:
-#                 unmatched_configs[k] = v
-#         if convert_back:
-#             opt = vars(opt)
-#     return opt, unmatched_configs
-
-
 def parse_yaml(path):
     import yaml
     with open(path, 'r', encoding='UTF-8') as stream:
@@ -100,6 +30,8 @@ def parse_list(opt, k, v):
 
         # format data
         data = (json.loads(v) if isinstance(v, str) else v)
+        if not isinstance(data, list):
+            data = [data, data]
         value = [content_type(val) for val in data]
     except:
         print('problems')
@@ -114,7 +46,7 @@ def parse_types(opt, k, v):
         elif _type is type(None):
             value = v
         elif _type == bool:
-            value = bool(distutils.util.strtobool(v))
+            value = bool(distutils.util.strtobool(v)) if type(v) != bool else v
         else:
             value = type(getattr(opt, k))(v)
     except:
@@ -125,11 +57,10 @@ def parse_types(opt, k, v):
 
 def format_key_value(opt, k, v, **kwargs):
     value = parse_types(opt, k, v)
-
     if k == 'batch_size':
         value = max(1, value)  # '16'
     if k == 'project':
-        value = f'{kwargs["data_path"]}/{v[1:] if v.startswith("/") else v}'
+        value = f'{kwargs["data_path"]}'  # /{v[1:] if v.startswith("/") else v}'
     if k == 'workers':
         value = max(0, value)
     if k == 'epochs':
@@ -145,21 +76,70 @@ def format_key_value(opt, k, v, **kwargs):
     return value
 
 
-def check_opts(opt, custom_cfg=None, data_path='data'):
-    unmatched_configs = custom_cfg
+# def check_opts(opt, custom_cfg=None, data_path='data'):
+#     unmatched_configs = custom_cfg
+#     if custom_cfg:
+#         configs = get_configs(custom_cfg)
+#         unmatched_configs = {}
+#
+#         kwargs = {'data_path': data_path}
+#         for k, v in configs.items():
+#             k = k.replace('-', '_')
+#             if hasattr(opt, k):
+#                 value = format_key_value(opt, k, v, **kwargs)
+#                 setattr(opt, k, value)
+#             else:
+#                 unmatched_configs[k] = v
+#     return opt, unmatched_configs
 
+
+def body_v1(opt, configs, **kwargs):
+    unmatched_configs = {}
+    for k, v in configs.items():
+        k = k.replace('-', '_')
+        if k == 'imgsz':
+            k = 'img_size'
+        if hasattr(opt, k):
+            value = format_key_value(opt, k, v, **kwargs)
+            if vars(opt)[k] != value:
+                print(f'key = {k}\tcurrent_key = {vars(opt)[k]}\tnew_key = {value}')
+                setattr(opt, k, value)
+        else:
+            unmatched_configs[k] = v
+
+    return opt, unmatched_configs
+
+
+def body_v2(opt, configs, **kwargs):
+    unmatched_configs = {}
+    opt_keys = set(vars(opt).keys())
+    configs_keys = set(configs.keys())
+    intersected_keys = list(opt_keys.intersection(configs_keys))
+    for key in intersected_keys:
+        k = key.replace('-', '_')
+        if hasattr(opt, k):
+            value = format_key_value(opt, k, configs[k], **kwargs)
+            if vars(opt)[k] != value:
+                print(f'key = {k}\t\tcurrent_key = {vars(opt)[k]}\t\tnew_key = {value}')
+                setattr(opt, k, value)
+        else:
+            unmatched_configs[k] = configs[k]
+
+    return opt, unmatched_configs
+
+
+def check_opts(opt, custom_cfg=None, data_path='data', version=1):
+    # if version == 1:
+    #     return check_opts_v1(opt, custom_cfg=custom_cfg, data_path=data_path)
+    # elif version == 2:
+    #     return check_opts_v2(opt, custom_cfg=custom_cfg, data_path=data_path)
+
+    unmatched_configs = custom_cfg
     if custom_cfg:
         configs = get_configs(custom_cfg)
-        unmatched_configs = {}
-
         kwargs = {'data_path': data_path}
-        for k, v in configs.items():
-            k = k.replace('-', '_')
-            if hasattr(opt, k):
-                value = format_key_value(opt, k, v, **kwargs)
-                setattr(opt, k, value)
-            else:
-                unmatched_configs[k] = v
+        body_v = body_v1 if version == 1 else body_v2
+        opt, unmatched_configs = body_v(opt, configs, **kwargs)
     return opt, unmatched_configs
 
 
